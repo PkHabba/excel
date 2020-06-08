a67f03a1d72e24200aea209c7a580bb3a5d0ae17
PkHabba <pkhabba@gmail.com>
2020-06-08 11:23:41 +0300

    Revert "Finished resize table"

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,24 +1,10 @@
 import {ExcelComponent} from '@core/ExcelComponent'
-import {createTable} from '@/components/table/table.template'
-import {resizeHadler} from '@/components/table/table.resize'
-import {shouldResize} from '@/components/table/table.functions'
+import {createTable} from '@/components/table/table.template';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
-  constructor($root) {
-    super($root, {
-      listeners: ['mousedown']
-    })
-  }
-
   toHTML() {
-    return createTable(20)
-  }
-
-  onMousedown(event) {
-    if (shouldResize(event)) {
-     resizeHadler(this.$root, event)
-    }
+    return createTable()
   }
 }
